e7d1f92ae136064367e6851080f5db22ba2ac698
tulies <346461062@qq.com>
2020-05-29 18:34:27 +0800

    feat: 新增git-cz

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-const { series} = require('gulp')
-import rollup from 'rollup'
+// import rollup from 'rollup'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import json from '@rollup/plugin-json'
 import babel from '@rollup/plugin-babel'
+// const { series } = require('gulp')
 // import camelCase from 'lodash.camelcase'
 // import babel from "rollup-plugin-babel";
 const pkg = require('./package.json')
@@ -12,38 +12,38 @@ const pkg = require('./package.json')
 const libraryName = 'xrvplayer'
 
 export default {
-    input: `src/${libraryName}.ts`,
-    output: [
-      { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
-      { file: pkg.module, format: 'es', sourcemap: true },
-    ],
-    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-    external: [],
-    watch: {
-      include: 'src/**',
-    },
-    plugins: [
-      // Allow json resolution
-      json(),
-      // Compile TypeScript files
-      // typescript({ useTsconfigDeclarationDir: true }),
-      
-      babel({
-        babelHelpers: 'runtime',
-        // exclude: 'node_modules/**',
-        // exclude: [/\/core-js\//],
-        // include: [
-        //   'src/**'
-        // ],  
-        extensions:["tsx","ts","js","jsx"]//超级关键配置
-      }),
-      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-      commonjs(),
-      // Allow node_modules resolution, so you can use 'external' to control
-      // which external modules to include in the bundle
-      // https://github.com/rollup/rollup-plugin-node-resolve#usage
-      resolve(),
-      // Resolve source maps to the original source
-      sourceMaps(),
-    ],
-  }
+  input: `src/${libraryName}.ts`,
+  output: [
+    { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true }
+  ],
+  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  external: [],
+  watch: {
+    include: 'src/**'
+  },
+  plugins: [
+    // Allow json resolution
+    json(),
+    // Compile TypeScript files
+    // typescript({ useTsconfigDeclarationDir: true }),
+
+    babel({
+      babelHelpers: 'runtime',
+      // exclude: 'node_modules/**',
+      // exclude: [/\/core-js\//],
+      // include: [
+      //   'src/**'
+      // ],
+      extensions: ['tsx', 'ts', 'js', 'jsx']// 超级关键配置
+    }),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+    // Resolve source maps to the original source
+    sourceMaps()
+  ]
+}
--- src/xrvplayer.ts
+++ src/xrvplayer.ts
@@ -1,15 +1,14 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-  export default class XrvPlayer {
-
-    public xxx(){
-      const v = ""
-      console.log(v)
-    }
-    public getArr(arr:number[]) {
-        console.log([...[1,2,3]])
-      return Array.from(new Set([1, 4, 2, 3, 4]))
-    }
+// import "core-js/fn/array.find"
+// ...
+export default class XrvPlayer {
+  public xxx () {
+    const v = ''
+    console.log(v)
   }
-  
+
+  public getArr (arr: number[]) {
+    console.log([...[1, 2, 3]])
+    return Array.from(new Set([1, 4, 2, 3, 4]))
+  }
+}
